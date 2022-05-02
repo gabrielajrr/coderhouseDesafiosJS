433586f7e9874e567ef9083d737ab52ac24dd643
Gabriela Rodriguez <gabrielajrr@gmail.com>
2022-05-02 20:43:03 -0300

    Desafio 3: Incorporar Arrays

---
+++ Desafio3. Incorporar Arrays/script.js
@@ -0,0 +1,78 @@
+//
+//
+//
+/*********** Desafio 3: Incorporar Arrays ***********/
+//
+//
+// CLASES
+
+class NuevoUsuario {
+    constructor(usuario, password, presupuestoDisponible) {
+        this.usuario = usuario;
+        this.password = password;
+        this.presupuestoDisponible = Number(presupuestoDisponible);
+    }
+    presupuesto() {
+        return `Hola ${this.usuario} tu presupuesto disponible es de ${this.presupuestoDisponible}.`;
+    }
+}
+
+class PrediccionUsuario {
+    constructor(nombrePiloto1, nombrePiloto2, nombrePiloto3, equipo) {
+        this.nombrePiloto1 = nombrePiloto1;
+        this.nombrePiloto2 = nombrePiloto2;
+        this.nombrePiloto3 = nombrePiloto3;
+        this.equipo = equipo;
+    }
+    mostrarPredicciones() {
+        return `Tus pilotos elegidos son:\npiloto1: ${this.nombrePiloto1}, \npiloto2: ${this.nombrePiloto2} y \npiloto3${this.nombrePiloto3}. \nEl nombre de tu equipo es: ${this.equipo}.`;
+    }
+}
+
+//
+//
+// VARIABLES
+//
+//----creando nuevo usuario con funcion crearUsuario
+const nuevoUsuario = crearUsuario();
+console.log(nuevoUsuario)
+
+//----creando preidiccion del usuario con funcion crearPrediccion
+const nuevaPrediccion = crearPrediccion();
+
+//----creando arrays con predicciones del usuario pusheando la constante nuevaPrediccion
+const prediccion = [];
+
+//
+//
+//FUNCIONES
+//
+//
+//---- funcion crear usuario, va a variable nuevoUsuario
+function crearUsuario() {
+    alert(`Bienvenid@ a FANtasyOne, por favor inicia sesion:`);
+    const usuario = prompt(`Ingresa tu nombre de usuario`).toLocaleLowerCase();
+    const password = prompt(`Ingresa tu contraseña`).toLocaleLowerCase();
+    const presupuestoDisponible = 1000000;
+
+    return new NuevoUsuario(usuario, password, presupuestoDisponible);
+}
+//
+//---- funcion crear prediccion, va a variable nuevaPrediccion
+function crearPrediccion() {
+    const nombrePiloto1 = prompt(`Ingresa el Nombre del Piloto 1`).toLocaleUpperCase();
+    const nombrePiloto2 = prompt(`Ingresa el Nombre del Piloto 2`).toLocaleUpperCase();
+    const nombrePiloto3 = prompt(`Ingresa el Nombre del Piloto 3`).toLocaleUpperCase();
+    const equipo = prompt(`Ingresa el Nombre del Equipo`).toLocaleUpperCase();
+
+    return new PrediccionUsuario(nombrePiloto1, nombrePiloto2, nombrePiloto3, equipo);
+}
+
+//
+//
+// INVOCACION DE FUNCIONES
+//
+//
+prediccion.push(nuevaPrediccion);
+console.log(prediccion);
+
